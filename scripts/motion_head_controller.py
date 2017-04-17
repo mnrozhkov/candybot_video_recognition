18334345f059dccbf635821b1c52aacd7e52d868
Mikhail Rozhkov <mnrozhkov@Mikhails-MacBook-Pro-2.local>
2017-04-17 21:41:04 +0300

    Re-commit changes in motion: servo, tests

--- scripts/motion_head_controller.py
+++ scripts/motion_head_controller.py
@@ -5,103 +5,11 @@
 coffebot head motion controller
 """
 
-import wiringpi
-import time
-import math
-
-#use BCM pin numbers
-wiringpi.wiringPiSetupGpio()
-
-# setup WiringPi PWM
-SERVO_PIN = 18
-PWM_DIVISOR = 384 # clock at 50kHz (20us tick)
-PWM_RANGE = 1000  # range at 1000 ticks (20ms)
-
-# setup pin as an output
-wiringpi.pinMode(SERVO_PIN, 2)
-wiringpi.pwmSetMode(0)
-wiringpi.pwmSetClock(PWM_DIVISOR)
-wiringpi.pwmSetRange(PWM_RANGE)
-wiringpi.pwmWrite(SERVO_PIN, 0) #theretically 50 (1ms) to 100 (2ms) on my servo 40-200 works ok
-
 import rospy
+
 from coffebot.msg import HeadMotion, HeadState
-
+from coffebot.motion.head.head_controller import Head
 from coffebot.topic_controller import Lock
-
-
-class Head(object):
-
-    def __init__(self):
-
-        #horizontal and vertical angles
-        self._h_angle = 0
-        self._v_angle = 0
-
-        #other settings
-        self._emotion = 'neutral'
-
-
-    def set_horizontal_servo_position(h_pos):
-        """
-        Turn servo on specified angle in degrees
-        Params:
-            pos: angle to turn servo, in degrees
-        """
-
-        h_move = int(40 + math.floor(h_pos * (200 - 40) / 180))
-        print("pos: {}".format(h_pos))
-        print("move: {}".format(h_move))
-
-        if h_move > 0:
-            print("move: ", h_move)
-            try:
-                wiringpi.pwmWrite(SERVO_PIN, h_move)
-                self._h_angle = h_move
-                # time.sleep(0.5)
-                # print("wiringpi.pwmWrite(18,{})".format(move))
-            except:
-                # clean up
-                wiringpi.pwmWrite(18, 0)
-                print("Exception")
-
-    def set_vertical_servo_position(v_pos):
-        """
-        Turn servo on specified angle in degrees
-        Params:
-            pos: angle to turn servo, in degrees
-        """
-
-        v_move = int(40 + math.floor(v_pos * (200 - 40) / 180))
-        print("pos: {}".format(v_pos))
-        print("move: {}".format(v_move))
-
-        if v_move > 0:
-            print("move: ", v_move)
-            try:
-                wiringpi.pwmWrite(SERVO_PIN, v_move)
-                self._v_angle = v_move
-                # time.sleep(0.5)
-                # print("wiringpi.pwmWrite(18,{})".format(move))
-            except:
-                # clean up
-                wiringpi.pwmWrite(18, 0)
-                print("Exception")
-
-
-    def get_head_position(self):
-        return (self._h_angle, self._v_angle)
-
-
-    def set_emotion(self, emotion=None):
-        if emotion is None:
-            self._emotion = 'neutral'
-        else:
-            self._emotion = emotion
-
-
-    def get_emotion(self):
-        return self._emotion
 
 
 def main():
@@ -112,7 +20,17 @@ def main():
     rospy.Subscriber('head_motion', headMotion, head_motion_lock.callback)
     head_position_publisher = rospy.Publisher('head_state')
 
-    head = Head()
+    head = Head(
+       _h_angle = 0,
+       _v_angle = 0,
+       _led_on = True,
+       _color = '#ffffff',
+       _emotion = 'neutral',
+       _h_SERVO_PIN = 18,
+       _v_SERVO_PIN = 18,
+       _PWM_DIVISOR = 384,      # clock at 50kHz (20us tick)
+       _PWM_RANGE = 1000,       # range at 1000 ticks (20ms)
+    )
 
     while True:
 
