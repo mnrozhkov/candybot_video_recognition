78ef42cbb764ec6368947628ea752f95bb534a98
Alexander <MeisterUrian@mail.ru>
2017-05-21 16:12:11 +0900

    add exception handling and logging into social/vk.py

--- src/coffebot/social/vk.py
+++ src/coffebot/social/vk.py
@@ -4,6 +4,15 @@ import vk
 import time
 import random
 
+import logging
+import os
+LOG_FOLDER = 'logs'
+if os.path.exists(LOG_FOLDER) is False:
+    os.mkdir(LOG_FOLDER)
+
+logging.basicConfig(filename=LOG_FOLDER + '/' + __name__ + '.log', format='[%(asctime)s] %(message)s\n\n',
+                    level=logging.DEBUG)
+
 
 class VkNeewsfeedScanner:
     '''
@@ -17,10 +26,14 @@ class VkNeewsfeedScanner:
             access_token: vk access token
             required_hashtag: required hashtag
         '''
-
+        self._session_opened = False
         self.access_token = access_token
         self.required_hashtag  = required_hashtag
-        self._open_session()
+        try:
+            self._open_session()
+            self._session_opened = True
+        except Exception as e:
+            logging.error(str(e))
 
     def _open_session(self) -> None:
         '''
@@ -42,20 +55,22 @@ class VkNeewsfeedScanner:
     		hashtag += str(random.randint(0,10))
     	return hashtag
 
-    def search_hashtag(self, hashtag: str) -> bool:
+    def search_hashtag(self, hashtag: str) -> bool or None:
         '''
         Search post with specified hashtag among news from newsfeed
         Args:
             hashtag: additional hashtag
         Returns:
             True : if post with required and additional tags found
-            False: in other case
+            False: if post with required and additional tags not found
+            None: could not take access to vk api
         '''
-
-    	nf = self._api.newsfeed.search(q=hashtag)
-    	if(nf[0] > 0):
-    		return True
-    	return False
+        if self._session_opened is True:
+        	nf = self._api.newsfeed.search(q=hashtag)
+        	if(nf[0] > 0):
+        		return True
+    	    return False
+        return None
 
     def listen(hashtag: str, timeout: int) -> bool:
         '''
@@ -64,12 +79,18 @@ class VkNeewsfeedScanner:
             hashtag: additional hashtag
         Returns:
             True : if post with required and additional tags found
-            False: in other case
+            False: if post with required and additional tags not found
+            None: could not take access to vk api
         '''
 
     	start = time.time()
     	while time.time() - start < timeout:
-    		if self.search_hashtag(hashtag) is True:
+            search_result = self.search_hashtag(hashtag)
+
+            if search_result is None:
+                return None
+
+    		if search_result is True:
     			return True
     		time.sleep(0.1)
 
