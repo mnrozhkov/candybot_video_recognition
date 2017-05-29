699c599ea0f2b00b7a2d0afe00a02728baeacc03
Alexander <MeisterUrian@mail.ru>
2017-05-29 13:52:41 +0900

    add functions upload_face_2_gallery and identify_face in vision/utils/findface.py

--- src/coffebot/vision/utils/findface.py
+++ src/coffebot/vision/utils/findface.py
@@ -38,7 +38,60 @@ def verify_faces(img1: bytes, img2: bytes) -> float or None:
     try:
         r = requests.post(url=url, files=files, headers=header)
         if r['verified'] is True:
-            return r['results']['confidence']
+            return r['results'][0]['confidence']
     except Exception as e:
         logging.error(str(e))
         return None
+
+def upload_face_2_gallery(img: bytes) -> int or None:
+    '''
+    take face image and upload it to findface.pro gallery
+    Args:
+        img: face image binary data
+    Returns:
+        img id in gallery : if uploaded
+        None : if failed
+    '''
+
+    url = 'https://api.findface.pro/v0/face'
+    header = {
+        'Host': 'api.findface.pro',
+        'Authorization': 'Token ' + token
+        }
+
+    files = {'photo': img}
+    
+    try:
+        r = requests.post(url=url, files=files, headers=header)
+        return r['results'][0]['id']
+    except Exception as e:
+        logging.error(str(e))
+        return None
+
+
+def identify_face(img: bytes) -> float or None:
+    '''
+    take face image and search it in findface.pro galleries
+    Args:
+        img: face image binary data
+    Returns:
+        confidence: if uploaded
+        None : if failed
+    '''
+
+    url = 'https://api.findface.pro/v0/identify'
+    header = {
+        'Host': 'api.findface.pro',
+        'Authorization': 'Token ' + token
+        }
+
+    files = {'photo': img}
+    
+    try:
+        r = requests.post(url=url, files=files, headers=header)
+        results = r['results']
+        return results[ list(results.keys())[0] ]['confidence']
+    except Exception as e:
+        logging.error(str(e))
+        return None
+    
