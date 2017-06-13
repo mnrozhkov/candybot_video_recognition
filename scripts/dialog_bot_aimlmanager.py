3e86b75a94e3903b1b060c0d30fcf44340088fa2
Alexander <MeisterUrian@mail.ru>
2017-06-13 12:52:56 +0900

    add exception hanling and logging in scripts/dialog_bot_aimlmanager.py

--- scripts/dialog_bot_aimlmanager.py
+++ scripts/dialog_bot_aimlmanager.py
@@ -18,6 +18,14 @@ from utils.topic_controller import Lock
 
 import time
 
+import logging
+LOG_FOLDER = 'logs'
+if os.path.exists(LOG_FOLDER) is False:
+    os.mkdir(LOG_FOLDER)
+
+logging.basicConfig(filename=LOG_FOLDER + '/' + __name__ + '.log', format='[%(asctime)s] %(message)s\n\n',
+                    level=logging.DEBUG)
+
 
 if __name__ == '__main__':
 
@@ -44,18 +52,20 @@ if __name__ == '__main__':
         user_speech_text_msg = lock_bot_request.message
         print('user text in bot: ', user_speech_text_msg)
         if isinstance(user_speech_text_msg, UserSpeechText):
-            bot_respond = bot.respond(user_speech_text_msg.text)
-            print('bot_respond: ', bot_respond)
-            bot_answer = json.loads(bot_respond)
-            print('aimlbot_answer:', bot_answer)
-            print('\n\n\n\n BOT!!!', type(bot_answer), '\n\n\n\n ')
-            if isinstance(bot_answer, dict):
-                bot_answer_msg = APIAIBotAnswer()
-                bot_answer_msg.text = bot_answer['text']
-                bot_answer_msg.action_name = bot_answer['action']['name']
-                bot_answer_msg.action_parameters_in_json = json.dumps(bot_answer['action']['parameters'])
+            try:
+                bot_respond = bot.respond(user_speech_text_msg.text)
+                print('bot_respond: ', bot_respond)
+                bot_answer = json.loads(bot_respond)
+                if isinstance(bot_answer, dict):
+                    bot_answer_msg = APIAIBotAnswer()
+                    bot_answer_msg.text = bot_answer['text']
+                    bot_answer_msg.action_name = bot_answer['action']['name']
+                    bot_answer_msg.action_parameters_in_json = json.dumps(bot_answer['action']['parameters'])
 
-                bot_decision_publisher.publish(bot_answer_msg)
+                    bot_decision_publisher.publish(bot_answer_msg)
+            except Exception as e:
+            	print('dialog_bot_aimlmanager: error: ', str(e))
+                logging.error(str(e))
 
         if lock_bot_request.message == user_speech_text_msg:
             lock_bot_request.message = None
